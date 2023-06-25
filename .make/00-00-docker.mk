6556769201b6ce7afbf42e660e2da69b2778f2d5
Asapdotid <asapdotid@gmail.com>
2023-06-25 13:25:20 +0700

    ⚡️ IMPROVE: change PWD to dot current root directory

--- .make/00-00-docker.mk
+++ .make/00-00-docker.mk
@@ -10,7 +10,7 @@ DOCKER_SERVICE_DATABASE_NAME:=mariadb
 # $(DOCKER_IMAGE_NAME)  --------------------------^      ^    mariadb
 # $(DOCKER_IMAGE_TAG)   ---------------------------------^    10.11.3
 
-DOCKER_DIR:=${PWD}/src
+DOCKER_DIR:=./src
 DOCKER_ENV_FILE:=$(DOCKER_DIR)/.env
 DOCKER_COMPOSE_FILE:=$(DOCKER_DIR)/compose.yml
 DOCKER_COMPOSE_DEV_FILE:=$(DOCKER_DIR)/compose.dev.yml
